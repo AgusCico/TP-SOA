910f47c1c7e6c468e0bc632337c3771f261e7500
Tadeo <tadeocofone@gmail.com>
2023-06-13 03:21:03 -0300

    Diseño mejorado y segunda activity creada con shake funcionando

--- SmartParking/app/src/main/java/com/m5/smartparking/MainActivity.java
+++ SmartParking/app/src/main/java/com/m5/smartparking/MainActivity.java
@@ -2,6 +2,7 @@ package com.m5.smartparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +18,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonSend = (Button) findViewById(R.id.buttonSend);
+        Button buttonSensor = (Button) findViewById(R.id.buttonSensor);
+
+        buttonSensor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openActivity2();
+            }
+        });
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sendMessage(view);
             }
         });
+
+
     }
 
+    public void openActivity2() {
+        Intent intent = new Intent(this, SensorsActivity.class);
+        startActivity(intent);
+    }
     public void sendMessage(View view){
         TextView textRequest = (TextView) findViewById(R.id.request);
         TextView textResponse = (TextView) findViewById(R.id.response);
